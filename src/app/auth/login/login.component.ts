776252078d15208858df694990ac350987c37069
ricardosantanadev <ricardosantanadev@gmail.com>
2024-05-13 21:29:06 -0300

    update

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,62 @@
-import { Component } from '@angular/core';
-import { NbLoginComponent } from '@nebular/auth';
+import { ChangeDetectorRef, Component, Inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { getDeepFromObject, NbAuthResult, NbAuthService, NbLoginComponent, NB_AUTH_OPTIONS } from '@nebular/auth';
 
 @Component({
   selector: 'ngx-login',
   templateUrl: './login.component.html',
 })
 export class NgxLoginComponent extends NbLoginComponent {
+
+  redirectDelay: number = 0;
+  showMessages: any = {};
+  strategy: string = '';
+
+  errors: string[] = [];
+  messages: string[] = [];
+  user: any = {};
+  submitted: boolean = false;
+  // socialLinks: NbAuthSocialLink[] = [];
+  rememberMe = false;
+
+  constructor(protected service: NbAuthService,
+              @Inject(NB_AUTH_OPTIONS) protected options = {},
+              protected cd: ChangeDetectorRef,
+              protected router: Router) {
+
+    super(service, options, cd, router);
+    this.redirectDelay = this.getConfigValue('forms.login.redirectDelay');
+    this.showMessages = this.getConfigValue('forms.login.showMessages');
+    this.strategy = this.getConfigValue('forms.login.strategy');
+    this.socialLinks = this.getConfigValue('forms.login.socialLinks');
+    this.rememberMe = this.getConfigValue('forms.login.rememberMe');
+  }
+
+  login(): void {
+    this.errors = [];
+    this.messages = [];
+    this.submitted = true;
+
+    this.service.authenticate(this.strategy, this.user).subscribe((result: NbAuthResult) => {
+      this.submitted = false;
+
+      if (result.isSuccess()) {
+        this.messages = result.getMessages();
+      } else {
+        this.errors = result.getErrors();
+      }
+
+      const redirect = result.getRedirect();
+      if (redirect) {
+        setTimeout(() => {
+          return this.router.navigateByUrl(redirect);
+        }, this.redirectDelay);
+      }
+      this.cd.detectChanges();
+    });
+  }
+
+  getConfigValue(key: string): any {
+    return getDeepFromObject(this.options, key, null);
+  }
 }
